a48db6138beba28e0333b6d838e6a79cc2c8b3ed
James Benjamin <james.abenj@gmail.com>
2019-10-18 04:09:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
-    end
+    else
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
 
