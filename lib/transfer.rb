466097b71601cb515a7edb3ecfde06c6e05bc65c
James Benjamin <james.abenj@gmail.com>
2019-10-18 04:15:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if sender.balance >= amount && self.status == "pending" 
+    if self.valid? && sender.balance >= amount && self.status == "pending" 
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete"
